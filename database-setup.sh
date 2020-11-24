9951aaa0f2ac9e78f35a708cb6217f71c393a698
Alexei Ozerov <o3esound@yandex.ru>
2020-11-23 19:29:12 -0500

    Update script with more detail

--- database-setup.sh
+++ database-setup.sh
@@ -7,4 +7,5 @@ echo "Your IP is: ${LOCALIP}"
 # Check for and if not existant, create data directory
 mkdir -p data
 
-echo "Please start the stack with docker-compose up -d, wait until active and ./data/ is populated. Then, run docker-compose down, and continue to add the above ip to your pg_fba.conf file."
+# Verbal instructions for next steps while I decide on the best way to automate this step ...
+echo "Please start the stack with docker-compose up -d, wait until active and ./data/ is populated. Then, run docker-compose down, and continue to add the above ip to your ./data/var/lib/postgresql/data/pg_fba.conf file."
